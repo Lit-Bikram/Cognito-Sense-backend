5642d96e95862ac47a492b4bb5e39d3531ed7e2d
Bikram <pal.bikram2004@gmail.com>
2026-02-05 02:27:52 +0530

    made some changes

--- src/googleDrive.ts
+++ src/googleDrive.ts
@@ -1,40 +1,10 @@
 import { google } from "googleapis";
 
-const KEY_PATH = "./drive-key.json"; 
-
-// 👉 PASTE YOUR DRIVE FILE ID HERE (from the CSV link)
-const DRIVE_FILE_ID = "1DFE622vTKIdQEOhJ981-eO8hQoxiNmPS";
+const DRIVE_FILE_ID = "1iHK454hK03GaRSgT4kHZ_TWc19m0QRzh";
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: KEY_PATH,
+  credentials: JSON.parse(process.env.GOOGLE_DRIVE_KEY as string),
   scopes: ["https://www.googleapis.com/auth/drive"],
 });
 
 const drive = google.drive({ version: "v3", auth });
-
-export async function appendRowToDriveCSV(row: string) {
-  try {
-    // Download current file content
-    const response = await drive.files.get({
-      fileId: DRIVE_FILE_ID,
-      alt: "media",
-    });
-
-    const existingCSV = response.data as unknown as string;
-
-    const updatedCSV = existingCSV + "\n" + row;
-
-    // Upload updated file
-    await drive.files.update({
-      fileId: DRIVE_FILE_ID,
-      media: {
-        mimeType: "text/csv",
-        body: updatedCSV,
-      },
-    });
-
-    console.log("✅ Row appended to Google Drive");
-  } catch (err) {
-    console.error("❌ Drive upload failed:", err);
-  }
-}
